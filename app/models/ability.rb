e36541e5cb331b14a9cad52b466963afe2edf287
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-22 14:03:10 +0200

    Fix linter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
-      user ||= User.new # guest user (not logged in)
-      if user.id == current_user.id
-        can :manage, :all
-      end
+    user ||= User.new # guest user (not logged in)
+    can :manage, :all if user.id == current_user.id
   end
 end
