ac6b73a26624406ee9dee343711c572a721ed2fe
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-22 11:32:07 +0200

    Add shoulda_matcher gem to easily test the models associations ans validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { User.new(name:"Polina", email: "polina@mail.com", password: "123456")}
+
+    before { user.save }
+
+    describe 'assocciations' do
+      it { should have_many(:moneytracks) }
+
+      it { should have_many(:groups) }
+    end
+
+    describe 'validations' do
+      it { should validate_presence_of(:name) }
+    end
 end
