f4ba8bd19cafc14deb9bcc73d6df05f5ee2af869
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-22 13:04:54 +0200

    Create MoneyTrack model with assocciations and validation

---
+++ app/models/money_track.rb
@@ -0,0 +1,7 @@
+class MoneyTrack < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
+
+  validates :name, presence: :true, length: {maximum: 50}
+  validates :amount, presence: :true, numericality: { only_integer: true, greater_than: 0 }
+end
